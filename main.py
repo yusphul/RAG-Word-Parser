23511f3c23f2640de5cf23a23a3d8beb8d08fdaa
adejumoyusuf@gmail.com <adejumoyusuf@gmail.com>
2026-01-04 12:54:52 -0600

    fix main.py error

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ class DocxParser:
         
         return chunks
     
-    def chunk_text(self, text: str) -> List[Dict]:
+    def process_document(self, file_path: str) -> List[Dict]:
         """
         Complete pipeline: parse DOCX and create chunks.
         
